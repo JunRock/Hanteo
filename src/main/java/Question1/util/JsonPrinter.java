c9e23ef28436e9fecb823bf8c1f3c7cc7c550d6f
JunRock <wnstjr120422@naver.com>
2025-05-19 15:00:56 +0900

    refactor : JSON 출력 포맷 변경

--- src/main/java/Question1/util/JsonPrinter.java
+++ src/main/java/Question1/util/JsonPrinter.java
@@ -1,5 +1,7 @@
 package Question1.util;
 
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -9,9 +11,14 @@ import lombok.RequiredArgsConstructor;
 public class JsonPrinter {
     private final ObjectMapper mapper;
 
-    public String toJson(Object o) {
+    public String toJson(Object object) {
         try {
-            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(o);
+            DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
+            DefaultIndenter indenter = new DefaultIndenter("    ", DefaultIndenter.SYS_LF);
+            prettyPrinter.indentArraysWith(indenter);
+            prettyPrinter.indentObjectsWith(indenter);
+
+            return mapper.writer(prettyPrinter).writeValueAsString(object);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
